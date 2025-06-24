625a575ae0ede489e44ca77dec4ab6f0b4563021
bms560 <bmsullivan560@gmail.com>
2025-06-24 01:47:33 -0400

    b

--- temp_ui_project/jest.config.js
+++ temp_ui_project/jest.config.js
@@ -4,25 +4,12 @@ const config = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   transform: {
-    '^.+\.(ts|tsx)$': ['ts-jest', {
-      tsconfig: 'tsconfig.app.json',
-      isolatedModules: true
-    }]
+    '^.+\\.(ts|tsx)$': 'ts-jest'
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '\.(css|less|scss|sass)$': 'identity-obj-proxy'
-  },
-  modulePaths: ['<rootDir>/src'],
-  moduleDirectories: ['node_modules', 'src'],
-  testPathIgnorePatterns: ['/node_modules/'],
-  testTimeout: 10000,
-  forceExit: true,
-  globals: {
-    'import.meta.env.VITE_API_BASE_URL': 'http://localhost:8000'
-  },
-  verbose: true
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  }
 };
 
 export default config;
