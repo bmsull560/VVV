21716988267279caf5a9da9f115d45d56c736191
bms560 <bmsullivan560@gmail.com>
2025-06-24 01:18:04 -0400

    nb

--- temp_ui_project/jest.config.js
+++ temp_ui_project/jest.config.js
@@ -1,19 +1,22 @@
 export default {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom-sixteen',
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
 
+  globals: {
+    'import.meta.env.VITE_API_BASE_URL': 'http://localhost:8000',
+  },
   roots: ['<rootDir>/src'],
   testMatch: [
-    '**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '**/*.(test|spec).{js,jsx,ts,tsx}'
+    '**/*.test.{ts,tsx}',
+    '**/*.spec.{ts,tsx}'
   ],
   transform: {
-    '^.+\\.(ts|tsx)?$': ['ts-jest', { useESM: true, tsconfig: 'tsconfig.app.json' }],
-    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)?$': ['ts-jest', { tsconfig: 'tsconfig.app.json' }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleDirectories: ['node_modules', 'src'],
+  modulePaths: ['<rootDir>/src'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
@@ -26,7 +29,8 @@ export default {
     '!src/vite-env.d.ts'
   ],
   transformIgnorePatterns: [
-    'node_modules/(?!(.*\\.mjs$))'
+    '/node_modules/',
+    '\.pnp\.[^\/]+$'
   ],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html']
